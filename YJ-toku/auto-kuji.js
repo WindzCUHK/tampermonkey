e7b7c68725fcf8bb68831b4f8164e3dfa07b079a
WindzCUHK <windz@link.cuhk.edu.hk>
2019-08-16 10:34:17 +0900

    update

--- YJ-toku/auto-kuji.js
+++ YJ-toku/auto-kuji.js
@@ -2,7 +2,7 @@
 // @name         YJ-toku, auto kuji
 // @description  auto click kuji button
 // @match        https://toku.yahoo.co.jp/*/lot*
-// @version      1.0.2
+// @version      1.0.3
 // @namespace    https://github.com/WindzCUHK/tampermonkey
 // @author       Windz
 // @downloadURL  https://raw.githubusercontent.com/WindzCUHK/tampermonkey/master/YJ-toku/auto-kuji.js
@@ -13,8 +13,12 @@
 	'use strict';
 
 	// auto click
-	const btn = $('#btnLot');
-	if (btn) btn.click();
+	const input = $('.btnActionKeyword__inputText');
+	if (!input) {
+		// prevent refresh for keyword input
+		const btn = $('#btnLot');
+		if (btn) btn.click();
+	}
 
 	// remove promo banner
 	const banner = document.querySelector(".textCenter.pcBox > .marginB20");
