46192dbfca554e966f791d0866fc3fb668f906e5
wfan <wfan@lycorp.co.jp>
2023-12-06 14:25:18 +0900

    d-point kuji close

--- D-point/auto-kuji.js
+++ D-point/auto-kuji.js
@@ -2,7 +2,7 @@
 // @name         D-point, auto kuji
 // @description  auto click kuji button
 // @match        https://kuji.dmarket.docomo.ne.jp/kuji/*
-// @version      1.0.2
+// @version      1.0.3
 // @namespace    https://github.com/WindzCUHK/tampermonkey
 // @author       Windz
 // @downloadURL  https://raw.githubusercontent.com/WindzCUHK/tampermonkey/master/D-point/auto-kuji.js
@@ -11,18 +11,39 @@
 // ==/UserScript==
 
 (function() {
-	'use strict';
+	"use strict";
 
 	const checkExist = setInterval(() => {
-		const img = document.querySelector('.lotteryArea img');
+		const img = document.querySelector(".lotteryArea img");
 		if (window.location.pathname !== "/kuji/ErT7HDcs") {
 			// auto kuji
 			img.click();
+
+			// close windows
+			setTimeout(() => {
+				const result = document.getElementById('lottery_result');
+				if (result) {
+					window.close();
+				}
+			}, 1000);
+
+			// send message to parent
+			// window.parent.postMessage("lottery DONE");
+			// window.opener.postMessage("lottery DONE");
+
+			// listen popup message
+			// window.addEventListener("message", (event) => {
+			// 	console.log("got:", event)
+			// 	if (event.data !== "hi from main window!") return;
+				
+			// 	console.log("sent message")
+			// 	event.source.postMessage("lottery DONE", '*');
+			// });
 		} else {
 			// re-layout
-			document.getElementById('complete-lotteries').style.cssText = 'top: 500px; position: absolute; width: 100%; background: white; zoom: 0.5;';
-			document.getElementById('floating-banner').style.display = 'none';
-			document.getElementById('sample').style.display = 'none';
+			document.getElementById("complete-lotteries").style.cssText = "top: 500px; position: absolute; width: 100%; background: white; zoom: 0.5;";
+			document.getElementById("floating-banner").style.display = "none";
+			document.getElementById("sample").style.display = "none";
 		}
 		if (img) clearInterval(checkExist);
 	}, 500);
