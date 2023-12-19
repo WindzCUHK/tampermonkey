dc240ee41ad45fdc75ce999b3960fcea779242c4
wfan <wfan@lycorp.co.jp>
2023-12-20 02:30:48 +0900

    version up

--- pointi/magazine.js
+++ pointi/magazine.js
@@ -3,7 +3,7 @@
 // @icon         https://www.google.com/s2/favicons?domain=pointi.jp
 // @description  auto click to the end
 // @match        https://pointi.jp/contents/magazine/*
-// @version      1.0.8
+// @version      1.0.9
 // @namespace    https://github.com/WindzCUHK/tampermonkey
 // @author       Windz
 // @downloadURL  https://raw.githubusercontent.com/WindzCUHK/tampermonkey/master/pointi/magazine.js
@@ -41,79 +41,70 @@
 	}
 
 	// auto forward
-	function extractValue(text) {
-		if (text.indexOf("stamp_box") !== -1) {
-			throw new Error("END"); // for old specification
-		}
-		if (text.indexOf("スタンプは付与済みです") !== -1) {
-			throw new Error("END");
-		}
-
-		const target = "movePage('";
-		const startIdx = text.indexOf(target, 0);
-
-		if (startIdx < 0) {
-			throw new Error("no target");
-		}
-		const endIdx = text.indexOf("='", startIdx);
-		return text.substring(startIdx + target.length, endIdx);
-	}
 	function closeSelf(err) {
 		if (err.message === "END") {
 			console.log("✅");
 			document.getElementById("outer").style.backgroundColor = "green";
+			// close tab
+			window.opener = self;
+			setTimeout(window.close, 1000);
 		} else {
 			console.error(err);
+			document.getElementById("outer").style.backgroundColor = "red";
 			// refresh tab to prevent error redirect
-			setTimeout(() => {
-				window.location.href = window.location.href;	
-			}, 1000);
+			setTimeout(() => { window.location.href = window.location.href; }, 1000);
 		}
-		window.opener = self;
-		setTimeout(window.close, 1000);
 	}
-	let nextValue;
-	async function next(value) {
-		await fetch(window.location.href, {
-			"headers": {
-				"accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
-				"accept-language": "en-US,en;q=0.9,zh-CN;q=0.8,zh;q=0.7,ja;q=0.6",
-				"cache-control": "max-age=0",
-				"content-type": "application/x-www-form-urlencoded",
-				"sec-ch-ua": "\" Not A;Brand\";v=\"99\", \"Chromium\";v=\"96\", \"Google Chrome\";v=\"96\"",
-				"sec-ch-ua-mobile": "?0",
-				"sec-ch-ua-platform": "\"macOS\"",
-				"sec-fetch-dest": "document",
-				"sec-fetch-mode": "navigate",
-				"sec-fetch-site": "same-origin",
-				"sec-fetch-user": "?1",
-				"upgrade-insecure-requests": "1"
-			},
-			"referrer": window.location.href,
-			"referrerPolicy": "strict-origin-when-cross-origin",
-			"body": `a=${encodeURIComponent(value)}%3D`,
-			"method": "POST",
-			"mode": "cors",
-			"credentials": "include"
+	async function next() {
+		// document.querySelector('title + script + script')
+		const searchParams = new URLSearchParams(window.location.search);
+		const subID = searchParams.get("sub");
+		const postID = searchParams.get("no");
+		const tokenURL = `/ajax_load/load_magazine_detail.php?sub=${subID}&no=${postID}&name=next&code=88818263152dct4qojnn40kksijt`;
+
+		// get token
+		await fetch(tokenURL, { "method": "GET" })
+		.then(response => response.text())
+		// get next page
+		.then(token => {
+			return fetch(window.location.href, {
+				"method": "POST",
+				"body": `token=${token}`,
+				"referrer": window.location.href,
+				"referrerPolicy": "strict-origin-when-cross-origin",
+				"mode": "cors",
+				"credentials": "include",
+				"headers": {
+					"cache-control": "max-age=0",
+					"content-type": "application/x-www-form-urlencoded",
+				},
+			});
 		})
 		.then(response => response.arrayBuffer())
-		.then((buffer) => {
+		.then(buffer => {
 			// decode japanese before text search
 			const decoder = new TextDecoder("shift-jis");
 			const text = decoder.decode(buffer);
-			nextValue = extractValue(text);
-			console.log("value: ", nextValue);
-			next(nextValue);
+			if (text.indexOf("stamp_box") !== -1) {
+				throw new Error("END"); // for old specification
+			}
+			if (text.indexOf("スタンプは付与済みです") !== -1) {
+				throw new Error("END");
+			}
+
+			setTimeout(next, 100); // recursion with delay
 		})
 		.catch((err) => {
 			closeSelf(err);
 		});
 	}
-	const movePage = document.getElementById("move_page");
-	if (movePage) {
-		next(extractValue(movePage.parentNode.innerHTML));
+	// auto forward main
+	if (document.getElementsByClassName("detail_date").length !== 0) {
+		// article page, start auto forward
+		next();
 	}
 	if (document.body.innerText.indexOf("スタンプは付与済みです") !== -1) {
+		// single page article
 		closeSelf(new Error("END"));
 	}
 
